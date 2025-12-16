e1eda870aff5741ec1534a83cb176d1740527556
Iván <ivan.corrales.solera@gmail.com>
2025-12-16 06:05:16 +0100

    Clean unused files

--- app/src/main/java/es/fpsumma/dam2/api/data/remote/dto/TareaCreateRequest.kt
+++
@@ -1,6 +0,0 @@
-package es.fpsumma.dam2.api.data.remote.dto
-
-data class TareaCreateRequest(
-    val titulo: String,
-    val descripcion: String
-)
--- app/src/main/java/es/fpsumma/dam2/api/data/remote/dto/TareaDTO.kt
+++
@@ -1,7 +0,0 @@
-package es.fpsumma.dam2.api.data.remote.dto
-
-data class TareaDTO(
-    val id: Int,
-    val titulo: String,
-    val descripcion: String
-)
